ffb6e032240d84778b98756d3abab89ef9493d6d
Nazima Bhat <bhatnazima141@gmail.com>
2026-01-29 21:46:28 +0530

    Initial project setup

---
+++ playwright.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+
+  use: {
+    baseURL: 'https://the-internet.herokuapp.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+  },
+
+  reporter: [
+    ['html', { open: 'never' }]
+  ],
+
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' }},
+    { name: 'Firefox', use: { browserName: 'firefox' }},
+    { name: 'WebKit', use: { browserName: 'webkit' }}
+  ]
+});
