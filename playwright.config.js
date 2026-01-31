789ee58afe3f4eb958cce37e8103404e53515b6f
Nazima Bhat <bhatnazima141@gmail.com>
2026-01-31 15:18:11 +0530

    Initial project setup

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30000,
+  timeout: 300000,
 
   use: {
     baseURL: 'https://the-internet.herokuapp.com',
